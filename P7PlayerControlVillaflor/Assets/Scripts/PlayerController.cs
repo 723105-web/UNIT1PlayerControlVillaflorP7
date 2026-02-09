9a4537a8f3d8016d19380da415d0a41a9d0c6019
D1luffy <723105@student.dusd.net>
2026-02-09 15:24:13 -0800

    Unit 1
    I added a second car and was able to have them move the same way liek they are one.

--- P7PlayerControlVillaflor/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlVillaflor/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public Camera mainCamera; 
     public Camera hoodCamera; 
     public KeyCode switchKey;
+    public string inputID;
 
 
 
@@ -21,8 +22,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // Rotates the car based on horizontal input
